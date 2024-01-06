0c950d700cef83c27708bd970ea7ce350e0d74f2
Corey Cothrum <coreycothrum@melloyawn.com>
2024-01-06 15:19:26 -0600

    feat: init functionality
    resolves #1: yes this is possible... but currently only on nightly

--- README.md
+++ README.md
@@ -4,11 +4,27 @@
 [![Documentation](https://img.shields.io/docsrs/nagme?logo=docs.rs)](https://docs.rs/nagme)
 [![License](https://img.shields.io/badge/license-MIT-blue?style=flat-square)](LICENSE-MIT)
 
-super pedantic linting that is easy to enabled & easy to maintain
+super pedantic, highly maintainable, linting
+
+Relieve yourself from the mental fatigue of deciding which lints to enabled for each and every crate.
+
+Just use `nagme`. All crates will have a curated, uniform set of very pedantic lints.
+
+# Note: `nagme` uses unstable features
+`nagme` requires nightly to build
+(see [rust-lang](https://github.com/rust-lang/rust)
+[issue #54726](https://github.com/rust-lang/rust/issues/54726))
 
 ## Usage
-1. Lorem ipsum dolor sit amet
+1. Add the `nagme` attribute macro to your crate:
    ```rust
+   #![nagme::nagme]
+   ```
+
+Until this crate can exist on stable rust, these additional macros will be required:
+   ```rust
+   #![feature(custom_inner_attributes)]
+   #![feature(prelude_import)]
    ```
 
 ## Documentation
--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,51 @@
-//! #FIXME
+//! super pedantic linting with a good developer experience
+//!
+//! Easily enable a currated set of lints.
+//! Relieve yourself from the burden of maintaining lints across all your crates.
+//!
+//! # Note: `nagme` uses unstable features
+//! `nagme` requires nightly to build
+//! (see [rust-lang](https://github.com/rust-lang/rust)
+//! [issue #54726](https://github.com/rust-lang/rust/issues/54726))
 //!
 //! # Examples
 //! ```
-//! // #FIXME
+//! # #![feature(custom_inner_attributes)]
+//! # #![feature(prelude_import)]
+//! #![nagme::nagme]
+//!
+//! fn main() {
+//!     assert!(true);
+//! }
 //! ```
 
 #![no_std]
+#![feature(proc_macro_quote)]
 
-use proc_macro::TokenStream;
+use proc_macro::{quote, TokenStream};
 
-#[proc_macro]
-pub fn nagme(_item: TokenStream) -> TokenStream {
-    concat!("").parse().unwrap()
+#[proc_macro_attribute]
+pub fn nagme(_attr: TokenStream, items: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let mut lints = quote! {
+        #![warn(
+            clippy::all,
+            clippy::cargo,
+            clippy::complexity,
+            clippy::nursery,
+            clippy::pedantic,
+            clippy::perf,
+            clippy::style,
+            clippy::unwrap_used,
+        )]
+        #![deny(clippy::suspicious)]
+        #![forbid(clippy::correctness)]
+
+        #![warn(
+            missing_docs,
+            unreachable_pub,
+            unused_crate_dependencies,
+        )]
+    };
+    lints.extend(items);
+    lints
 }
